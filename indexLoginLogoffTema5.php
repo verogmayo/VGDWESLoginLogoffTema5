529a36f50db592444b1f42ae77a14775e096fff3
Véro Grué <veronique.gru@educa.jcyl.com>
2025-11-19 22:21:11 +0100

    Add Añadir carpetas y fichero para crear el proyecto

---
+++ indexLoginLogoffTema5.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Véro Grué - DWESLoginLogoffTema5</title>
+        <!--Fuente de google font-->
+        <!--Para descargar iconos. https://v2.boxicons.com/usage  (import the css)-->
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="webroot/css/styles.css">
+        <!--https://cdnjs.com/libraries/font-awesome --> 
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <header class="header">
+            <a href="../VGDWESProyectoDWES/indexProyectoDWES.html">volver</a>
+            <h1>PROYECTO LOGIN LOGOFF TEMA 5</h1>
+        </header>
+        <main>
+            <section>
+
+                <h3>CONTENIDO DE LOS FICHEROS EXTERNOS</h3>
+                <table>
+                    <tr>
+                        <th>Descripción</th>
+                        <th>Desarrollo</th>
+                        <th>Explotación</th>
+                        
+                        <th>Descriptción</th>
+                        <th>Desarrollo</th>
+                        <th>Explotación</th>
+                    </tr>
+                    <tr>
+                        <td class="texto">Creación de la base de Datos y del usuario. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraCreaDBVGDWESLoginLogoffTema5.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                        <td class="iconos" rowspan="2" > <a href="mostrarcodigo/muestraDBVGDWESLoginLogoffTema5.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                        <td class="texto">Librería personal. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraMiLibreriaStatic.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraMiLibreriaStatic.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                    </tr>
+
+                    <tr>
+
+                        <td class="texto">Carga de la base de Datos. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraCargaDBVGDWESLoginLogoffTema5.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+<!--                         <td class="iconos"> <a href="mostrarcodigo/muestraDBVGDWESLoginLogoffTema5.php"><i class="fa-solid fa-eye"></i> </a>  </td>-->
+                        <td class="texto">Fichero de configuración PDO. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraconfDBPDODes.php"><i class="fa-solid fa-eye"></i> </a></td>
+                         <td class="iconos"> <a href="mostrarcodigo/muestraconfDBPDO.php"><i class="fa-solid fa-eye"></i> </a></td>
+                    </tr>
+                    <tr>
+                        <td class="texto">Borrado de la base de Datos. </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraBorraDBVGDWESLoginLogoffTema5.php"><i class="fa-solid fa-eye"></i> </a>  </td>
+                        <td class="texto"> </td>
+                        <td class="iconos"> <a href=""><!--<i class="fa-solid fa-eye"></i>--> </a>  </td>
+                    </tr>
+
+
+                </table>
+
+                <table>
+                    <tr>
+                        <th rowspan="2">Nº</th>
+                        <th rowspan="2">Enunciados</th>
+                        <th colspan="2">PDO</th>
+                    </tr>
+                    <tr>
+                        <th>Ejecutar</th>
+                        <th>Mostrar</th>
+                    </tr>
+                    <tr>
+                        <th>0</th>
+<!--                    <span class="span">text</span> -->
+                    <td class="texto">Desarrollo de una aplicación (Proyecto LoginLogoffTema5) con control de acceso e identificación
+del usuario basado en un formulario  </td>
+                    <td class="iconos"><a href="codigoPHP/ejercicio00.php"><!--<i class="fa-solid fa-circle-play"> </i>--> </a>  </td>
+                    <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio00.php"> <!--<i class="fa-solid fa-eye"></i>--> </a>  </td>
+                   
+                    </tr>
+
+                    <tr>
+                        <th>1</th>
+                        <!-- <span class="span">text</span> -->
+
+                        <td class="texto"> </td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio01.php"> <!--<i class="fa-solid fa-circle-play"> </i>--> </a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio01.php"> <!--<i class="fa-solid fa-eye"></i>--> </a>  </td>
+                    </tr>
+                    <tr>
+                        <th>2</th>
+                        <td class="texto"></td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio02.php"> <!--<i class="fa-solid fa-circle-play"> </i>--></a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio02.php"> <!--<i class="fa-solid fa-eye"></i>--> </a>  </td>
+                    </tr>
+<!--                    <tr>
+                        <th>3</th>
+                        <td class="texto">Desarrollo de una aplicación (Proyecto LoginLogoffTema5) con control de acceso e identificación
+                            del usuario basado en un formulario (Login.php).</td>
+                        <td class="iconos"><a href="codigoPHP/ejercicio03.php"><i class="fa-solid fa-circle-play"></i></a>  </td>
+                        <td class="iconos"> <a href="mostrarcodigo/muestraEjercicio03.php"><i class="fa-solid fa-eye"></i></a>  </td>
+
+                    </tr>-->
+                   
+                </table>
+            </section>
+        </main>
+        <footer class="footer">
+            <div class="footerContent">
+                                    <div class="social-media">
+                        <a href="https://github.com/verogmayo/VGDWESLoginLogoffTema5"><i class='bx bxl-github' ></i></a>
+                    </div>
+                <div><p class="copyright">
+                        2025-26 IES LOS SAUCES. &#169;Todos los derechos reservados.</p> <address><a href="../index.html">Véronique Grué.</a> Fecha de Actualización :
+                        <time datetime="2025-11-19"></time> 19-11-2025 </address>
+
+                    
+                </div>
+
+            </div>
+
+        </footer>
+
+    </body>
+</html>
